936871b23f3298dc2934af2081efa19738438768
ashujainvi <ajainvi@buffalo.edu>
2022-05-08 11:23:50 -0500

    Add new design for test.

--- app/js/app.js
+++ app/js/app.js
@@ -1,40 +1,51 @@
-var screenSize = window.innerWidth;
-var scene = document.getElementById('mouse-move-scene-1');
-var parallaxInstance = new Parallax(scene);
-// typed initiator
-// add typed js only if width > 768px
-// if (screenSize > 768) {
-// 	var head = document.getElementsByTagName('body')[0];
-// 	var scriptTag = document.createElement('script');
-// 	scriptTag.type = 'text/javascript';
-// 	scriptTag.src = 'https://cdn.jsdelivr.net/npm/typed.js@2.0.9';
-// 	head.appendChild(scriptTag);
-// }
+const docElement = document.documentElement;
+const rootElement = document.querySelector(':root');
+
+const COLORS = {
+  primary: '#cf042a',
+  red: '#cf042a',
+};
+// var screenSize = window.innerWidth;
+// PARALLAX
+// var scene = document.getElementById('mouse-move-scene-1');
+// var parallaxInstance = new Parallax(scene);
+
+// TYPED.JS
+// var head = document.getElementsByTagName('body')[0];
+// var scriptTag = document.createElement('script');
+// scriptTag.type = 'text/javascript';
+// scriptTag.src = 'https://cdn.jsdelivr.net/npm/typed.js@2.0.9';
+// head.appendChild(scriptTag);
 
 // var typed;
-// function initializeTyped() {
-// 	const stringArray = [
-// 		'~ ashutoshjainvi.com$: Hey ' + userName + '! Nice to meet you.',
-// 		'~ ashutoshjainvi.com$: So, let me help you in waking this sloth up.',
-// 		"~ ashutoshjainvi.com$: There's a white switch on top left corner of the desk.",
-// 		'~ ashutoshjainvi.com$: Click on it to turn the lights on.',
-// 		'~ ashutoshjainvi.com$: Be careful, it might break his concentration and make him mad.',
-// 	];
-// 	var typedOptions = {
-// 		strings: stringArray,
-// 		typeSpeed: 40,
-// 		backDelay: 700,
-// 		attr: 'placeholder',
-// 		loop: true,
-// 		onStop: function () {
-// 			typed.reset();
-// 		},
-// 	};
-// 	typed = new Typed('_____FILL IN THE BLANK', typedOptions);
+// function initializeTyped(elementId, typedInfo) {
+//   const stringArray = [
+//     '~ ashutoshjainvi.com$: Hey! Nice to meet you.',
+//     '~ ashutoshjainvi.com$: So, let me help you in waking this sloth up.',
+//     "~ ashutoshjainvi.com$: There's a white switch on top left corner of the desk.",
+//     '~ ashutoshjainvi.com$: Click on it to turn the lights on.',
+//     '~ ashutoshjainvi.com$: Be careful, it might break his concentration and make him mad.',
+//   ];
+//   var typedOptions = {
+//     strings: stringArray,
+//     typeSpeed: 40,
+//     backDelay: 700,
+//     loop: true,
+//     onStop: function () {
+//       typed.reset();
+//     },
+//   };
+//   typed = new Typed(elementId, typedOptions);
 // }
 
-// if (screenSize > 768) {
-// 	initializeTyped();
+// // POP-UP
+// function showPopup(elementId, typedPlaceholder, typedInfo) {
+//   const el = document.getElementById(elementId);
+//   el.classList.add('pop-up-open');
+
+//   setTimeout(function () {
+//     initializeTyped(typedPlaceholder, typedInfo);
+//   }, 375);
 // }
 
 // ICONS SLIDER
@@ -43,55 +54,143 @@ var mouseIsDown = false;
 var startX, scrollLeft;
 
 if (iconSlider) {
-    iconSlider.addEventListener('mousedown', function (event) {
-        mouseIsDown = true;
-        $('.feature_zone-icons').stop(true);
-        startX = event.pageX - iconSlider.offsetLeft;
-        scrollLeft = iconSlider.scrollLeft;
-    });
+  iconSlider.addEventListener('mousedown', function (event) {
+    mouseIsDown = true;
+    startX = event.pageX - iconSlider.offsetLeft;
+    scrollLeft = iconSlider.scrollLeft;
+  });
 
-    iconSlider.addEventListener('mouseleave', function () {
-        mouseIsDown = false;
-    });
+  iconSlider.addEventListener('mouseleave', function () {
+    mouseIsDown = false;
+  });
 
-    iconSlider.addEventListener('mouseup', function () {
-        mouseIsDown = false;
+  iconSlider.addEventListener('mouseup', function () {
+    mouseIsDown = false;
 
-        var currScrollX = iconSlider.scrollLeft;
-        var scrollDiff = (scrollLeft - currScrollX) * -1;
-        var newScrollX = currScrollX + scrollDiff * 0.4;
+    var currScrollX = iconSlider.scrollLeft;
+    var scrollDiff = (scrollLeft - currScrollX) * -1;
+    var newScrollX = currScrollX + scrollDiff * 0.4;
 
-        $('.feature_zone-icons')
-            .stop(true)
-            .animate({ scrollLeft: newScrollX }, 400, 'linear');
-    });
+    // TODO: animate not working
+    iconSlider.animate({ scrollLeft: newScrollX }, 400);
+  });
 
-    iconSlider.addEventListener('mousemove', function (event) {
-        if (!mouseIsDown) {
-            return;
-        }
-        var mousePosX = event.pageX - iconSlider.offsetLeft;
-        var slideValue = (mousePosX - startX) * 1.5;
-        iconSlider.scrollLeft = scrollLeft - slideValue;
-    });
+  iconSlider.addEventListener('mousemove', function (event) {
+    if (!mouseIsDown) {
+      return;
+    }
+    var mousePosX = event.pageX - iconSlider.offsetLeft;
+    var slideValue = (mousePosX - startX) * 1.5;
+    iconSlider.scrollLeft = scrollLeft - slideValue;
+  });
 }
 
-// uiKit animations using scrollSpy
-// var topServiceGrids = document.getElementById('top-service-grids');
-// var options = {
-// 	cls: 'animation-fade',
-// 	target: '> div',
-// 	delay: 220,
-// 	repeat: true,
-// };
-// UIkit.scrollspy(topServiceGrids, options);
+// CHAPTER ANIMATIONS BASED ON WINDOW SCROLL.
+// SHOWS OR HIDES STICKY CHAPTERS BASED ON SCROLL TOP.
 
-var sectionIntro = document.getElementsByClassName('section-intro');
-var sectionOptions = {
-    cls: 'animation-fade',
-    delay: 220,
-    repeat: true,
+// Pins for each chapter's end.
+// Example, Chapter 1 scroll ends at 500px (chapter fades away).
+// Chapter 2 starts at 500px, which tiggers:
+// - Chapter 1 past animation (see utility.scss `.past-chapter`).
+// - Chapter 2 present animation (see utility.scss `.present-chapter`).
+const CHAPTER_END_POINTS = [1000, 2200, 3500];
+
+// Contains different CSS props value for each chapter.
+// Can be used to dynamically change CSS props like background.
+const CHAPTER_PROPS = {
+  0: {
+    'background-color': '#cf042a',
+  },
+  1: {
+    'background-color': '#f76e48',
+  },
+  2: {
+    'background-color': '#cf042a',
+  },
 };
-for (var i = 0; i < sectionIntro.length; i++) {
-    UIkit.scrollspy(sectionIntro[i], sectionOptions);
-}
+
+// Get all chapters id="chapter-{i}" (where i starts from 0) for element of id="personal-book".
+// HTML Collection of all chapter elements.
+const chapters = document.getElementById('personal-book').children;
+const numberOfChapters = chapters.length;
+
+/**
+ * Return Chapter's CSS Property value.
+ * @param {number} chapterNumber Chapter index for desired CSS prop.
+ * @param {string} propertyName CSS prop name. For example, `background-color`.
+ * @returns {string} CSS property value.
+ */
+const getChapterProperty = (chapterNumber, propertyName) => {
+  return CHAPTER_PROPS[chapterNumber][propertyName];
+};
+
+// Returns chapter based on index provided.
+const getChapter = (chapterNumber) => {
+  if (chapterNumber < 0 || chapterNumber >= numberOfChapters) {
+    return undefined;
+  }
+  return chapters[chapterNumber];
+};
+
+const getCurrentChapterNumber = (scrollTop) => {
+  let chapterNumber = 0;
+
+  chapterNumber = CHAPTER_END_POINTS.find(
+    (chapterEndPoint) => scrollTop < chapterEndPoint
+  );
+
+  return CHAPTER_END_POINTS.indexOf(chapterNumber);
+};
+
+const animateChapter = (scrollTop, prevChapterNumber) => {
+  // chapter numbers start with 0 in our book.
+  const currentChapterNumber = getCurrentChapterNumber(scrollTop);
+
+  if (currentChapterNumber === prevChapterNumber) {
+    return currentChapterNumber;
+  }
+
+  // Get chapters to set CSS props on.
+  const prevChapter = getChapter(currentChapterNumber - 1);
+  const currentChapter = getChapter(currentChapterNumber);
+  const nextChapter = getChapter(currentChapterNumber + 1);
+
+  if (currentChapter) {
+    requestAnimationFrame(() => {
+      currentChapter.classList.remove('future-chapter');
+      currentChapter.classList.remove('past-chapter');
+      currentChapter.classList.add('present-chapter');
+
+      // Change default background color.
+      rootElement.style.setProperty(
+        '--background-color',
+        getChapterProperty(currentChapterNumber, 'background-color')
+      );
+    });
+  }
+
+  if (prevChapter) {
+    requestAnimationFrame(() => {
+      prevChapter.classList.remove('present-chapter');
+      prevChapter.classList.add('past-chapter');
+    });
+  }
+
+  // If there is a next chapter, which would be current when scrolling up.
+  if (nextChapter) {
+    requestAnimationFrame(() => {
+      nextChapter.classList.remove('present-chapter');
+      nextChapter.classList.add('future-chapter');
+    });
+  }
+
+  return currentChapterNumber;
+};
+
+// SCROLL EVENT
+let prevChapterNumber = 0;
+
+window.addEventListener('scroll', function (e) {
+  const scrollTop = docElement.scrollTop;
+  prevChapterNumber = animateChapter(scrollTop, prevChapterNumber);
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const { src, series, parallel, dest, watch } = require('gulp');
-// Importing all the Gulp-related packages
+// Importing all the Gulp related packages
 const sourcemaps = require('gulp-sourcemaps');
 const sass = require('gulp-sass');
 const concat = require('gulp-concat');
@@ -17,28 +17,28 @@ const del = require('del');
 const DEST_DIRECTORY = 'dist';
 
 const FILE_PATHS = {
-    html: {
-        src: 'app/*.html',
-        dest: DEST_DIRECTORY,
-    },
-    scss: {
-        src: 'app/scss/**/*.scss',
-        dest: `${DEST_DIRECTORY}/css`,
-        destFileName: 'styles.css',
-    },
-    js: {
-        src: ['app/js/vendor/**/*.js', 'app/js/*.js'],
-        dest: `${DEST_DIRECTORY}/js`,
-        destFileName: 'script.js',
-    },
-    images: {
-        src: 'app/images/**/*',
-        dest: `${DEST_DIRECTORY}/images`,
-    },
-    static: {
-        src: 'app/static/**/*',
-        dest: `${DEST_DIRECTORY}/static`,
-    },
+  html: {
+    src: 'app/*.html',
+    dest: DEST_DIRECTORY,
+  },
+  scss: {
+    src: 'app/scss/**/*.scss',
+    dest: `${DEST_DIRECTORY}/css`,
+    destFileName: 'styles.css',
+  },
+  js: {
+    src: ['app/js/vendor/**/*.js', 'app/js/*.js'],
+    dest: `${DEST_DIRECTORY}/js`,
+    destFileName: 'script.js',
+  },
+  images: {
+    src: 'app/images/**/*',
+    dest: `${DEST_DIRECTORY}/images`,
+  },
+  static: {
+    src: 'app/static/**/*',
+    dest: `${DEST_DIRECTORY}/static`,
+  },
 };
 
 /////////////////
@@ -47,102 +47,96 @@ const FILE_PATHS = {
 
 // SCSS TASK: creates a minified styles.css file in DESTINATION FOLDER
 function scssBuildTask() {
-    return src(FILE_PATHS.scss.src)
-        .pipe(gulpMode.development(sourcemaps.init())) // initialize sourcemaps first
-        .pipe(sass().on('error', sass.logError)) // compile SCSS to CSS
-        .pipe(concat(FILE_PATHS.scss.destFileName))
-        .pipe(gulpMode.production(postcss([autoprefixer(), cssnano()]))) // PostCSS plugins, PROD-ONLY
-        .pipe(sourcemaps.write('.')) // write sourcemaps file in current directory
-        .pipe(dest(FILE_PATHS.scss.dest)) // put final CSS in dist folder
-        .pipe(browserSync.stream());
+  return src(FILE_PATHS.scss.src)
+    .pipe(gulpMode.development(sourcemaps.init())) // initialize sourcemaps first
+    .pipe(sass().on('error', sass.logError)) // compile SCSS to CSS
+    .pipe(concat(FILE_PATHS.scss.destFileName))
+    .pipe(gulpMode.production(postcss([autoprefixer(), cssnano()]))) // PostCSS plugins, PROD-ONLY
+    .pipe(sourcemaps.write('.')) // write sourcemaps file in current directory
+    .pipe(dest(FILE_PATHS.scss.dest)) // put final CSS in dist folder
+    .pipe(browserSync.stream());
 }
 
 // JS: concatenates and uglifies JS files to script.js
 function jsTask() {
-    return src(FILE_PATHS.js.src)
-        .pipe(concat(FILE_PATHS.js.destFileName))
-        .pipe(gulpMode.production(uglify())) // PROD-ONLY
-        .pipe(dest(FILE_PATHS.js.dest));
+  return src(FILE_PATHS.js.src)
+    .pipe(concat(FILE_PATHS.js.destFileName))
+    .pipe(gulpMode.production(uglify())) // PROD-ONLY
+    .pipe(dest(FILE_PATHS.js.dest));
 }
 
 // Image: Compress image and move it to dist folder.
 function imageTask() {
-    return src(FILE_PATHS.images.src)
-        .pipe(
-            //PROD-ONLY
-            gulpMode.production(
-                imagemin(
-                    [
-                        imagemin.gifsicle({ interlaced: true }),
-                        imagemin.mozjpeg({ quality: 65, progressive: true }),
-                        imagemin.optipng({ optimizationLevel: 6 }),
-                        imagemin.svgo({
-                            plugins: [
-                                { removeViewBox: true },
-                                { cleanupIDs: true },
-                            ],
-                        }),
-                    ],
-                    {
-                        verbose: false,
-                    }
-                )
-            )
+  return src(FILE_PATHS.images.src)
+    .pipe(
+      // PROD-ONLY
+      gulpMode.production(
+        imagemin(
+          [
+            imagemin.gifsicle({ interlaced: true }),
+            imagemin.mozjpeg({ quality: 65, progressive: true }),
+            imagemin.optipng({ optimizationLevel: 6 }),
+            imagemin.svgo({
+              plugins: [{ removeViewBox: true }, { cleanupIDs: true }],
+            }),
+          ],
+          {
+            verbose: false,
+          }
         )
-        .pipe(dest(FILE_PATHS.images.dest));
+      )
+    )
+    .pipe(dest(FILE_PATHS.images.dest));
 }
 
 // STATIC COPY: Copy static items to dest folder
 function staticCopyTask() {
-    return src(FILE_PATHS.static.src).pipe(dest(FILE_PATHS.static.dest));
+  return src(FILE_PATHS.static.src).pipe(dest(FILE_PATHS.static.dest));
 }
 
 // CACHEBUST: replace '?cb={number}' string in html files to enable cache busting for css
 // Uses gulp replace to find ?cb and replace it with timestamp
 function cacheBustTask() {
-    var cbString = new Date().getTime(); // get new cb based on current time
-    return src(FILE_PATHS.html.src)
-        .pipe(replace(/cb=\d+/g, 'cb=' + cbString))
-        .pipe(dest(FILE_PATHS.html.dest));
+  var cbString = new Date().getTime(); // get new cb based on current time
+  return src(FILE_PATHS.html.src)
+    .pipe(replace(/cb=\d+/g, 'cb=' + cbString))
+    .pipe(dest(FILE_PATHS.html.dest));
 }
 
 // CLEAN BUILDS: Clean existing builds when serving or building.
 function cleanBuilds() {
-    return del(`${DEST_DIRECTORY}/**'`, { force: true });
+  return del(`${DEST_DIRECTORY}/**'`, { force: true });
 }
 
 // Watch task: watch SCSS, JS and HTML files for changes
 function watchTask() {
-    browserSync.init({
-        server: {
-            baseDir: `./${DEST_DIRECTORY}`,
-        },
-    });
-    watch(
-        [FILE_PATHS.scss.src],
-        { interval: 200, usePolling: true },
-        scssBuildTask
-    );
+  browserSync.init({
+    injectChanges: true,
+    https: true,
+    server: {
+      baseDir: `./${DEST_DIRECTORY}`,
+    },
+  });
 
-    watch([...FILE_PATHS.js.src], jsTask);
-    watch(
-        [FILE_PATHS.html.src],
-        { interval: 200, usePolling: true },
-        series(cacheBustTask, browserSync.reload)
-    );
+  watch([FILE_PATHS.scss.src], scssBuildTask);
+  watch([...FILE_PATHS.js.src], jsTask);
+  watch([FILE_PATHS.html.src]).on(
+    'change',
+    series(cacheBustTask, browserSync.reload)
+  );
 }
 
 // DEVELOPMENT SERVE
 exports.serve = series(
-    cleanBuilds,
-    parallel(scssBuildTask, jsTask, imageTask, staticCopyTask),
-    cacheBustTask,
-    watchTask
+  cleanBuilds,
+  parallel(scssBuildTask, jsTask, imageTask, staticCopyTask),
+  cacheBustTask,
+  series(watchTask)
 );
 
 // PROD BUILD
 exports.build = series(
-    cleanBuilds,
-    parallel(scssBuildTask, jsTask, imageTask, staticCopyTask),
-    cacheBustTask
+  cleanBuilds,
+  parallel(scssBuildTask, jsTask, imageTask, staticCopyTask),
+  cacheBustTask
 );
