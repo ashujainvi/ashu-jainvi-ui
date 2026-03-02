3966bb1f64d605e2a0bcead26881afae391718ab
Ashu Jainvi <ashujainvi@gmail.com>
2026-03-01 23:29:27 -0600

    test analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { useEffect } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useEffect, useRef } from 'react';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import './App.css';
 import Nav from './organisms/Nav/Nav';
 import Footer from './organisms/Footer/Footer';
@@ -9,6 +9,40 @@ import About from './pages/About/About';
 import Photos from './pages/Photos/Photos';
 import Contact from './pages/Contact/Contact';
 import PageTransition from './components/PageTransition/PageTransition';
+
+declare global {
+  interface Window {
+    gtag?: (...args: unknown[]) => void;
+  }
+}
+
+function usePageTracking() {
+  const location = useLocation();
+  const isFirstRender = useRef(true);
+  useEffect(() => {
+    if (isFirstRender.current) {
+      isFirstRender.current = false;
+      return; // gtag config in index.html handles the initial page view
+    }
+    window.gtag?.('event', 'page_view', {
+      page_path: location.pathname + location.search,
+    });
+  }, [location]);
+}
+
+function AppRoutes() {
+  usePageTracking();
+  return (
+    <PageTransition>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/photos" element={<Photos />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </PageTransition>
+  );
+}
 
 function App() {
   useEffect(() => {
@@ -21,14 +55,7 @@ function App() {
     <BrowserRouter>
       <ScrollToTop />
       <Nav />
-      <PageTransition>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/photos" element={<Photos />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </PageTransition>
+      <AppRoutes />
       <Footer />
     </BrowserRouter>
   );
