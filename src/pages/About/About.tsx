f1ff5dd7c7b5f44890b32f053f9c4048efe1f1b8
Ashu Jainvi <ashujainvi@gmail.com>
2026-03-19 17:15:54 -0500

    fix about me page

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -11,36 +11,33 @@ const About = () => (
       type="profile"
     />
     <PageHero overline="Get to know" title="About Me">
-      <section className={styles.prose}>
+      <section className="prose">
         <p>
-          Hey there fellow human, I am{' '}
-          <strong className={styles.highlight}>Ashu</strong>. I am professional
-          FrontEnd Engineer. My career started as an intern summer 2017 when I
-          joined <strong>DSW&rsquo;</strong>.
+          Hey there fellow human, I am <strong>Ashu</strong>. I am professional
+          FrontEnd Engineer, photographer and a soccer ethusiast from Austin,
+          TX.
         </p>
         <p>
-          After completing my master&rsquo;s degree in 2018, I joined DSW
-          full-time as a <strong>UI Engineer</strong> solving complex web app
-          problems like analytics, accessibility, AB testing, performance, and
-          SEO.
+          After finishing my M.S in Computer Science from University at Buffalo
+          in 2018, I worked at DSW as a <strong>UI Engineer</strong> solving
+          complex web problems like analytics, accessibility, AB testing,
+          performance, and SEO.
         </p>
         <p>
-          I continued working as a <strong>Senior developer</strong> at DSW till
-          2020 end. Working across on the backend &amp; on the front end.
+          At the end of 2022, I consulted at <strong>Apple</strong> as a{' '}
+          <strong>Senior UI Developer</strong>, where I used my expertise in
+          redesigning, rewriting, architecting, and documenting an Apple-wide
+          enterprise API management web platform.
         </p>
         <p>
-          At the end of 2022, I joined <strong>Apple</strong> as a{' '}
-          <strong>Senior UI Developer</strong> as a consultant, where I used my
-          expertise in architecting, documenting, redesigning, and rewriting an
-          Apple-wide enterprise API management web platform.
-        </p>
-        <p>
-          In February 2025, when my contract ended, I took a pause to reflect on
-          what brings me joy. I started focusing on integrating{' '}
-          <strong>AI tools</strong> into web projects with efficiency in mind,
-          and pursued long-lost hobbies like <em>soccer, photography</em>, and{' '}
-          <em>digital creativity</em>. I also spent good time with my family in
-          India.
+          In February 2025, I took a pause to reflect on what brings me joy. I
+          rekindled my love for family, community, photography and soccer
+          full-time. After spending time with my family and friends, I started a
+          new chapter of portrait photography and AI-assisted web development,
+          which motived my to start focusing on integrating{' '}
+          <strong>AI tools</strong> into web projects with efficiency in mind. I
+          am passionate about creating innovative solutions that leverage the
+          power of AI to enhance user experiences and drive business growth.
         </p>
         <p className={styles.note}>
           <em>
